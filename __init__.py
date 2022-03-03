9b7d823f89f8d547e39f1c858a85685979248f9e
ipeis <ipeis@tsc.uc3m.es>
2022-03-03 17:26:41 +0100

    Delete unnecessary init file

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  Copyright (c) 2022 by Ignacio Peis, UC3M.                                    +
-#  All rights reserved. This file is part of the HH-VAEM, and is released under +
-#  the "MIT License Agreement". Please see the LICENSE file that should have    +
-#  been included as part of this package.                                       +
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
